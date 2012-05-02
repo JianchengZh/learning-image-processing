5cdfe4b5bb08519b50f9239db865bac92edf65ff
Edwin Fernando Muñoz Delgado <edwinfernandomudelgado@hotmail.com>
2012-05-02 12:48:55 -0500

    Update

--- LOGIC/histogram.cpp
+++ LOGIC/histogram.cpp
@@ -50,9 +50,10 @@ Histogram::Histogram(ImagenPGM *imagen)
                 max2=max1;
                 max1=i;
             } else if(relativeFrecuency[i]>relativeFrecuency[max2]){
-                max2=i;
+                if((i-max1)>8)
+                    max2=i;
             }
-        cout<<max1<<" max2 "<<max2<<" i "<<i<<" relativeFrecuency[i] "<<relativeFrecuency[i]<<" relativeFrecuency[max2] "<<relativeFrecuency[max2]<<endl;
+            cout<<max1<<" max2 "<<max2<<" i "<<i<<" relativeFrecuency[i] "<<relativeFrecuency[i]<<" relativeFrecuency[max2] "<<relativeFrecuency[max2]<<endl;
         }
     }
 
