c73195d6e15a91b33b98a34128c6f519e481da84
Gustavo Adolfo Rodriguez <gustalibreros@gmail.com>
2012-06-13 17:07:21 -0500

    Cabeceras de los archivos actualizadas

--- CONTROLLER/maincontroller.cpp
+++ CONTROLLER/maincontroller.cpp
@@ -2,16 +2,21 @@
 // INTRODUCCION AL PROCESAMIENTO DIGITAL DE IMÁGENES
 // LEARNING_IMAGE_PROCESSING
 //
-// ARCHIVO: maincontroller.cpp
 //
 // FECHA INICIACION: Marzo de 2012
 //
 // AUTORES:
 // Gustavo Adolfo Rodriguez         0932979-3743
-// gustalibreros@hotmail.com
+// gustalibreros@gmail.com
 //
 // Edwin Fernando Muñoz             0910398-3743
-// edwinfernandomudelgado@hotmail.com
+// edwinfernandomudelgado@gmail.com
+//
+// Yerminson Doney Gonzalez         0843846-3743
+// yermigon@gmail.com
+//
+// Edgar Andrés Moncada             0832294-3743
+// edgarandres29@gmail.com
 //
 // ESCUELA DE INGENIERIA DE SISTEMAS Y COMPUTACION
 // UNIVERSIDAD DEL VALLE
